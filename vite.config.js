6a7e1eed89d131b2d1fc69cc871932ecfe5169ea
Transfera <gestaoestoqueapp@gmail.com>
2026-05-26 11:34:25 -0300

    corrige vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'icon-192.svg', 'icon-512.svg'],
+      includeAssets: ['favicon.ico', 'icon-192.png', 'icon-512.png'],
       manifest: {
         name: 'Transfera',
         short_name: 'Transfera',
@@ -19,9 +19,8 @@ export default defineConfig({
         scope: '/transfera/',
         start_url: '/transfera/',
         icons: [
-  { src: 'icon-192.png', sizes: '192x192', type: 'image/png' },
-  { src: 'icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' }
-]
+          { src: 'icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: 'icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' }
         ]
       }
     })

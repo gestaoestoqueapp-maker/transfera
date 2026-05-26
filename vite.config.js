10e4003a6f9c0cfc1b921aa9bc69f1f2697d9f22
Transfera <gestaoestoqueapp@gmail.com>
2026-05-26 11:33:05 -0300

    icones PNG para instalacao PWA

--- vite.config.js
+++ vite.config.js
@@ -19,8 +19,9 @@ export default defineConfig({
         scope: '/transfera/',
         start_url: '/transfera/',
         icons: [
-          { src: 'icon-192.svg', sizes: '192x192', type: 'image/svg+xml' },
-          { src: 'icon-512.svg', sizes: '512x512', type: 'image/svg+xml' }
+  { src: 'icon-192.png', sizes: '192x192', type: 'image/png' },
+  { src: 'icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' }
+]
         ]
       }
     })

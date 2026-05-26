96ede65d1c1e9e15356a1cbb26d9c379c5eb27f6
Transfera <gestaoestoqueapp@gmail.com>
2026-05-26 11:45:38 -0300

    corrige caminho dos icones PNG no manifest

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,10 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'icon-192.png', 'icon-512.png'],
+      includeAssets: ['favicon.svg', 'icon-192.png', 'icon-512.png'],
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      },
       manifest: {
         name: 'Transfera',
         short_name: 'Transfera',
@@ -19,8 +22,8 @@ export default defineConfig({
         scope: '/transfera/',
         start_url: '/transfera/',
         icons: [
-          { src: 'icon-192.png', sizes: '192x192', type: 'image/png' },
-          { src: 'icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' }
+          { src: '/transfera/icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/transfera/icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' }
         ]
       }
     })
